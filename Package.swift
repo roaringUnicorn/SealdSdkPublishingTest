62f23a53d6ad6239598e674d959c272b68df1e58
clement <clement@seald.io>
2024-10-02 18:18:07 +0200

    fix path

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SealdSdkPublishingTest",
-            path: "SealdSdk/Classes")
+            path: "SealdSdkPublishingTest/SealdSdk")
     ]
 )
