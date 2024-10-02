bbbdde97786b8c97d3b46ac9d40806112c742da2
clement <clement@seald.io>
2024-10-02 17:44:59 +0200

    fixing manifest

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SealdSdk",
-            path: "test-package-swift/SealdSdk/Classes"
-            dependencies: [])
+            path: "test-package-swift/SealdSdk/Classes")
     ]
 )
