0f48100bf1980c2ac25c6b50bc8efe39ebc0731e
clement <clement@seald.io>
2024-10-02 18:08:16 +0200

    renaming

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SealdSdk",
+    name: "SealdSdkPublishingTest",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SealdSdk",
+            name: "SealdSdkPublishingTest",
             targets: ["SealdSdk"]),
     ],
     dependencies: [
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SealdSdk",
-            path: "SealdSdk/Classes")
+            name: "SealdSdkPublishingTest",
+            path: "SealdSdkPublishingTest/Classes")
     ]
 )
