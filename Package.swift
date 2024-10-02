08ef42c246ece78835e850ed40c0e683a4a3f4a5
clement <clement@seald.io>
2024-10-02 17:37:23 +0200

    package file v2

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-test-pkg",
+    name: "SealdSdk",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swift-test-pkg",
-            targets: ["swift-test-pkg"]),
+            name: "SealdSdk",
+            targets: ["SealdSdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,11 +22,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swift-test-pkg",
-            path: "SealdSdk/Classes"
-            dependencies: []),
-        .testTarget(
-            name: "swift-test-pkgTests",
-            dependencies: ["swift-test-pkg"]),
+            name: "SealdSdk",
+            path: "test-package-swift/SealdSdk/Classes"
+            dependencies: [])
     ]
 )
