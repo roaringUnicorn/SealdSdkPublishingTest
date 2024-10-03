8f937caedfd267b3e0f0abeb56f1508b43fae4a4
clement <clement@seald.io>
2024-10-03 17:13:04 +0200

    fix paths

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SealdSdk",
-            targets: ["SealdSdkSwift", "SealdSdkObjC", "SealdSdkBinary"])
+            targets: ["SealdSdkSwift", "SealdSdkObjC"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,15 +22,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SealdSdkSwift",
-            path: "SealdSdk/swift"),
+            name: "SealdSdkSwift"),
         // .binaryTarget(
         //     name: "SealdSdkBinary",
-        //     path: "SealdSdk/objc/Frameworks/SealdSdkInternals.xcframework"
+        //     path: "Sources/SealdSdk/Frameworks/SealdSdkInternals.xcframework"
         // ),
         .target(
             name: "SealdSdkObjC",
-            path: "SealdSdk/objc",
-            publicHeadersPath: "SealdSdk/objc/Classes")
+            publicHeadersPath: "Sources/SealdSdk/Classes")
     ]
 )
