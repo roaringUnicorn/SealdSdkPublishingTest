c0835964b73fcdb14e1afaa66c8091be071be4ce
clement <clement@seald.io>
2024-10-03 17:24:32 +0200

    try stuff

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SealdSdk",
-            targets: ["SealdSdkSwift", "SealdSdkObjC"])
+            targets: ["SealdSdkObjC"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
